b0c7cd510368ada37a9d6f6caa59ff7d02b3e011
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-07-23 16:52:21 +0300

    function shuffle cards

--- src/Component/Game.jsx
+++ src/Component/Game.jsx
@@ -1,3 +1,5 @@
+import { array } from "prop-types";
+
 async function fetchCard(){
     const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=6');
     const data = await response.json();
@@ -6,9 +8,12 @@ async function fetchCard(){
         id:index,
         image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`,
         isFlipped:false,
-    }))
+    }));
 }
 
+const shuffleCard =(card) =>{
+    return card.sort(() => Math.random() - 0.5);
+}
 
 function GameBoard(){
 
