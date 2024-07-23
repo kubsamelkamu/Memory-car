f6a9d27f26c0101474e6f4e06afc3a204177feac
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-07-23 16:49:04 +0300

    method to fetchcard

--- src/Component/Game.jsx
+++ src/Component/Game.jsx
@@ -1,4 +1,13 @@
+async function fetchCard(){
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=6');
+    const data = await response.json();
 
+    return data.resultd.map((pokemon,index) =>({
+        id:index,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`,
+        isFlipped:false,
+    }))
+}
 
 
 function GameBoard(){
