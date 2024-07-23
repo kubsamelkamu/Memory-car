419b674e04f95b978cdd61e9c204f3f4089f238c
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-07-23 16:23:14 +0300

    jsx get returned added

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -1,3 +1,17 @@
+
+
 function DisplayCard({id,image,isFlipped,onClick}){
-        
+        return(
+            <div className={`card $ {isFlipped? 'flipped':''}`}
+             onClick={() => onClick(id)}
+            >
+              {isFlipped?<img src={image} alt="card"/>:'X' }
+            </div>
+        )
 }
+
+
+
+
+
+export default DisplayCard;
